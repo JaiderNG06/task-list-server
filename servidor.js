2269eff059e055ce3490a1a44beee19a85298645
Jaider alberto navarro gil <Jaideragnavarro.2003@gmail.com>
2024-01-08 23:01:20 -0500

    Subiendo mi primer contenido en la rama project-1

---
+++ servidor.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+const tareas = [
+    { id: 1, description: 'Hacer la compra', completed: false },
+    { id: 2, description: 'Estudiar para el examen', completed: true },
+    { id: 3, description: 'Hacer ejercicio', completed: false },
+  ];
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en el http://localhost:3000`);
+  });
+  
